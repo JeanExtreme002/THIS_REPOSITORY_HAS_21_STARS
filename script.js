810f0efe81220d34f0cac9b20c2b74a0bf5f6057
JeanExtreme002 <jeangamerextreme@gmail.com>
2021-08-31 22:09:31 -0300

    added README.md

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const repository = require("./repository");
+const fs = require("fs");
+
+function updateRepositoryName(newRepositoryName) {
+	repository.updateRepository({name: newRepositoryName});
+    console.log("Repository renamed to ->", newRepositoryName);
+}
+
+function updateReadMeFile(stars) {
+	const readMeTemplate = fs.readFileSync("README-Template.md").toString();
+	const readMeContent = readMeTemplate.replace("{stars}", stars);
+	const commitMessage = ":star: " + stars;
+	repository.writeFile("main", "./README.md", readMeContent, commitMessage);
+}
 
 repository.getDetails().then((details) => {
-	const stars = details.data.stargazers_count;
-	const newRepositoryName = "THIS_REPOSITORY_HAS_" + stars + "_STARS";
-	
-	repository.updateRepository({name: newRepositoryName});
-	console.log("Repository renamed to -> ", newRepositoryName);
+	updateReadMeFile(details.data.stargazers_count);
+    updateRepositoryName("THIS_REPOSITORY_HAS_" + details.data.stargazers_count; + "_STARS");
 });
