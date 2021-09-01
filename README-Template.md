fa2b77359378b0e654d6801ab0ce62e7d69c2902
JeanExtreme002 <jeangamerextreme@gmail.com>
2021-09-01 01:18:47 -0300

    added stargazers list

--- README-Template.md
+++ README-Template.md
@@ -1,13 +1,15 @@
 # THIS REPOSITORY HAS {stars} STARS :star:
 Click on :star: to see it in action :star_struck:
 
+##### <img width="60" src="{lastStargazerImage}"/> &nbsp; Thanks you [{lastStargazerName}](lastStargazerLink) for starring!
+
 # How does it work? :nerd_face:
 
 This is an application created in NodeJS using the [`github-api`](https://www.npmjs.com/package/github-api) package, which runs on Github Actions, theoretically, every 5 minutes or whenever a user performs some action in the repository.
 
 <br/>
 
-When the program runs, the github token and repository name are passed to the application and used to get access to the repository data. Then the repository is renamed and even this file is updated. 
+When the program runs, the github token and repository name are passed to the application and used to get access to the repository data. Then the repository is renamed and even this file is updated.
 
 <br/>
 
--- script.js
+++ script.js
@@ -1,19 +1,52 @@
 const repository = require("./repository");
 const fs = require("fs");
 
+function getRegisteredStargazers() {
+	return fs.readFileSync("./registered-stargazers.txt").toString().split(" ");
+}
+
+function updateReadMeFile(lastStargazerName, lastStargazerImage, lastStargazerLink, stars) {
+    const readMeTemplate = fs.readFileSync("./README-Template.md").toString();
+	const commitMessage = ":star: " + stars;
+
+	let readMeContent = readMeTemplate.replace(/{stars}/g, stars);
+	readMeContent = readMeContent.replace(/{lastStargazerName}/g, lastStargazerName);
+	readMeContent = readMeContent.replace(/{lastStargazerImage}/g, lastStargazerImage);
+	readMeContent = readMeContent.replace(/{lastStargazerLink}/g, lastStargazerLink);
+
+    repository.writeFile("main", "README.md", readMeContent, commitMessage);
+}
+
 function updateRepositoryName(newRepositoryName) {
-	repository.updateRepository({name: newRepositoryName});
+    repository.updateRepository({name: newRepositoryName});
     console.log("Repository renamed to ->", newRepositoryName);
 }
 
-function updateReadMeFile(stars) {
-	const readMeTemplate = fs.readFileSync("./README-Template.md").toString();
-	const readMeContent = readMeTemplate.replace(/{stars}/g, stars);
-	const commitMessage = ":star: " + stars;
-	repository.writeFile("main", "README.md", readMeContent, commitMessage);
+function updateStargazerDatabase(stargazers, lastStargazerName) {
+	const commitMessage = "Thank you " + lastStargazerName + " for starring.";
+	repository.writeFile("main", "registered-stargazers.txt", stargazers, commitMessage);
 }
 
-repository.getDetails().then((details) => {
-	updateReadMeFile(details.data.stargazers_count);
-    updateRepositoryName("THIS_REPOSITORY_HAS_" + details.data.stargazers_count + "_STARS");
-});
+async function main() {
+	const repositoryDetails = await repository.getDetails();
+	const stargazers_count = repositoryDetails.data.stargazers_count;
+
+	const registeredStargazers = getRegisteredStargazers();
+	const stargazers = await repository._request('GET', `/repos/${repository.__fullname}/stargazers`, null);
+	let lastStargazer;
+
+	stargazers.data.forEach((stargazer) => {
+		const stargazerID = stargazer.id.toString();
+
+        if (registeredStargazers.indexOf(stargazerID) == -1) {
+			registeredStargazers.push(stargazerID);
+		    lastStargazer = stargazer;
+		}
+	});
+
+	updateStargazerDatabase(registeredStargazers.join(" "), lastStargazer.login);
+	updateReadMeFile(lastStargazer.login, lastStargazer.avatar_url, lastStargazer.html_url, stargazers_count);
+    updateRepositoryName("THIS_REPOSITORY_HAS_" + stargazers_count + "_STARS");
+}
+
+main();
