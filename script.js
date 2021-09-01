ba83736653830ecb3498c03a5dd3292cb9010af6
JeanExtreme002 <jeangamerextreme@gmail.com>
2021-09-01 01:40:08 -0300

    removed registered-stargazers.txt

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 const repository = require("./repository");
 const fs = require("fs");
 
-function getRegisteredStargazers() {
-	return fs.readFileSync("./registered-stargazers.txt").toString().split(" ");
-}
-
 function updateReadMeFile(lastStargazerName, lastStargazerImage, lastStargazerLink, stars) {
     const readMeTemplate = fs.readFileSync("./README-Template.md").toString();
-	const commitMessage = ":star: " + stars;
+    const commitMessage = ":star: " + stars;
 
-	let readMeContent = readMeTemplate.replace(/{stars}/g, stars);
-	readMeContent = readMeContent.replace(/{lastStargazerName}/g, lastStargazerName);
-	readMeContent = readMeContent.replace(/{lastStargazerImage}/g, lastStargazerImage);
-	readMeContent = readMeContent.replace(/{lastStargazerLink}/g, lastStargazerLink);
+    let readMeContent = readMeTemplate.replace(/{stars}/g, stars);
+    readMeContent = readMeContent.replace(/{lastStargazerName}/g, lastStargazerName);
+    readMeContent = readMeContent.replace(/{lastStargazerImage}/g, lastStargazerImage);
+    readMeContent = readMeContent.replace(/{lastStargazerLink}/g, lastStargazerLink);
 
     repository.writeFile("main", "README.md", readMeContent, commitMessage);
 }
@@ -23,20 +19,19 @@ function updateRepositoryName(newRepositoryName) {
 }
 
 function updateStargazerDatabase(stargazers, lastStargazerName) {
-	const commitMessage = "Thank you " + lastStargazerName + " for starring.";
-	repository.writeFile("main", "registered-stargazers.txt", stargazers, commitMessage);
+    const commitMessage = "Thank you " + lastStargazerName + " for starring.";
+    console.log(stargazers, lastStargazerName);
+    repository.writeFile("main", "registered-stargazers.txt", stargazers, commitMessage);
 }
 
 async function main() {
-	const repositoryDetails = await repository.getDetails();
-	const stargazers_count = repositoryDetails.data.stargazers_count;
+    const repositoryDetails = await repository.getDetails();
+    const stargazers_count = repositoryDetails.data.stargazers_count;
 
-	const registeredStargazers = getRegisteredStargazers();
-	const stargazers = await repository._request('GET', `/repos/${repository.__fullname}/stargazers`, null);
-	const lastStargazer = stargazers.data[stargazers.data.length - 1];
+    const stargazers = await repository._request('GET', `/repos/${repository.__fullname}/stargazers`, null);
+    const lastStargazer = stargazers.data[stargazers.data.length - 1];
 
-	updateStargazerDatabase(registeredStargazers.join(" "), lastStargazer.login);
-	updateReadMeFile(lastStargazer.login, lastStargazer.avatar_url, lastStargazer.html_url, stargazers_count);
+    updateReadMeFile(lastStargazer.login, lastStargazer.avatar_url, lastStargazer.html_url, stargazers_count);
     updateRepositoryName("THIS_REPOSITORY_HAS_" + stargazers_count + "_STARS");
 }
 
