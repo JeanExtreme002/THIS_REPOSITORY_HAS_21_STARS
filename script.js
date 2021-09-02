85272b8a7a7a4fc3d7595b4cceecf8dbf172bbde
Jean Loui Bernard Silva de Jesus <45553309+JeanExtreme002@users.noreply.github.com>
2021-09-02 14:32:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const repository = require("./repository");
+const {repository, owner} = require("./repository");
 const fs = require("fs");
 
 function updateReadMeFile(lastStargazerName, lastStargazerImage, lastStargazerLink, stars) {
@@ -18,18 +18,14 @@ function updateRepositoryName(newRepositoryName) {
     console.log("Repository renamed to ->", newRepositoryName);
 }
 
-function updateStargazerDatabase(stargazers, lastStargazerName) {
-    const commitMessage = "Thank you " + lastStargazerName + " for starring.";
-    console.log(stargazers, lastStargazerName);
-    repository.writeFile("main", "registered-stargazers.txt", stargazers, commitMessage);
-}
-
 async function main() {
     const repositoryDetails = await repository.getDetails();
     const stargazers_count = repositoryDetails.data.stargazers_count;
 
     const stargazers = await repository._request('GET', `/repos/${repository.__fullname}/stargazers`, null);
-    const lastStargazer = stargazers.data[stargazers.data.length - 1];
+    
+    let lastStargazer = stargazers.data[stargazers.data.length - 1];
+    lastStargazer = lastStargazer.login == owner ? stargazers.data[stargazers.data.length - 2] : lastStargazer;
 
     updateReadMeFile(lastStargazer.login, lastStargazer.avatar_url, lastStargazer.html_url, stargazers_count);
     updateRepositoryName("THIS_REPOSITORY_HAS_" + stargazers_count + "_STARS");
