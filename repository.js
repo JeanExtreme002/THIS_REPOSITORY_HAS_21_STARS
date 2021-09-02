f865dc6df282b2bcdf303e4f1f4ef71626b9f76c
Jean Loui Bernard Silva de Jesus <45553309+JeanExtreme002@users.noreply.github.com>
2021-09-02 14:32:57 -0300

    Update repository.js

--- repository.js
+++ repository.js
@@ -3,7 +3,10 @@ const GitHub = require("github-api");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const github = new GitHub({token: process.env.TOKEN});
-const repository = github.getRepo(...process.env.REPOSITORY.split("/"));
+const githubToken = process.env.TOKEN;
+const [owner, repositoryName] = process.env.REPOSITORY.split("/");
 
-module.exports = repository;
+const github = new GitHub({token: githubToken});
+const repository = github.getRepo(owner, repositoryName);
+
+module.exports = {repository: repository, owner: owner};
